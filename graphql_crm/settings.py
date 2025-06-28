bc26b8c677a0669f7047e64c05fde1b531978a3b
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-06-28 04:09:58 +0100

    feat: add the "graphene_django" app

--- graphql_crm/settings.py
+++ graphql_crm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # THIRD-PARTY APPS
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
