7c5f73a392e0633ce73a23868e6ef38103555e26
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-06-29 23:22:36 +0100

    feat: created the Customer class

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unqiue=True, max_length=255)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
