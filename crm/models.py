fd762dff5f2d5ee9753e71497b0edee4791f94e9
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-06-29 23:23:55 +0100

    feat: added the Orders table

--- crm/models.py
+++ crm/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Customer(models.Model):
@@ -21,3 +22,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    product = models.ManyToManyField(Product, related_name='orders')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.name}"
