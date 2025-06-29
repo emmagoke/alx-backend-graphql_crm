167f59036b544cfddc286c5e351966cc5fdc5286
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-06-29 23:23:21 +0100

    feat: added the Product Table

--- crm/models.py
+++ crm/models.py
@@ -11,3 +11,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
