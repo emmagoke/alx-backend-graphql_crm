d3bfd0741e57332eabb6996bf311dc8e1a19730b
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-07-12 00:41:15 +0100

    feat: updated the schema to have filters

--- crm/schema.py
+++ crm/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 from django.db import transaction
@@ -7,6 +8,7 @@ import re
 from decimal import Decimal
 
 from .models import Customer, Product, Order
+from .filters import CustomerFilter, ProductFilter, OrderFilter
 
 # -- GraphQL Types --
 # Maps Django models to GraphQL types
@@ -14,31 +16,156 @@ class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
         fields = ('id', 'name', 'email', 'phone', 'created_at')
+        interfaces = (graphene.relay.Node,)
 
 
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
         fields = ('id', 'name', 'price', 'stock', 'created_at')
+        filter_fields = ['name', 'price', 'stock']
+        interfaces = (graphene.relay.Node,)
 
 
 class OrderType(DjangoObjectType):
     class Meta:
         model = Order
         fields = ('id', 'customer', 'products', 'total_amount', 'order_date', 'created_at')
+        filter_fields = ['customer', 'products', 'total_amount', 'order_date']
+        interfaces = (graphene.relay.Node,)
 
+
+# Filter Input Types
+class CustomerFilterInput(graphene.InputObjectType):
+    name_icontains = graphene.String()
+    email_icontains = graphene.String()
+    created_at_gte = graphene.DateTime()
+    created_at_lte = graphene.DateTime()
+    phone_pattern = graphene.String()
+    # CamelCase aliases for GraphQL queries
+    nameIcontains = graphene.String()
+    emailIcontains = graphene.String()
+    createdAtGte = graphene.DateTime()
+    createdAtLte = graphene.DateTime()
+    phonePattern = graphene.String()
+
+
+class ProductFilterInput(graphene.InputObjectType):
+    name_icontains = graphene.String()
+    price_gte = graphene.Float()
+    price_lte = graphene.Float()
+    stock_gte = graphene.Int()
+    stock_lte = graphene.Int()
+    low_stock = graphene.Int()
+    # CamelCase aliases for GraphQL queries
+    priceGte = graphene.Float()
+    priceLte = graphene.Float()
+    stockGte = graphene.Int()
+    stockLte = graphene.Int()
+    nameIcontains = graphene.String()
+    lowStock = graphene.Int()
+
+
+class OrderFilterInput(graphene.InputObjectType):
+    total_amount_gte = graphene.Float()
+    total_amount_lte = graphene.Float()
+    order_date_gte = graphene.DateTime()
+    order_date_lte = graphene.DateTime()
+    customer_name = graphene.String()
+    product_name = graphene.String()
+    product_id = graphene.ID()
+
+
+# Custom Connection Field
+class CustomerConnection(graphene.Connection):
+    class Meta:
+        node = CustomerType
+
+class ProductConnection(graphene.Connection):
+    class Meta:
+        node = ProductType
 
 # -- Query --
-# The basic query from the first question
 class Query(graphene.ObjectType):
-    all_customers = graphene.List(CustomerType)
-    all_products = graphene.List(ProductType)
-
-    def resolve_all_customers(self, info):
-        return Customer.objects.all()
+    all_customers = graphene.ConnectionField(CustomerConnection, filter=CustomerFilterInput())
+    all_products = graphene.ConnectionField(ProductConnection, filter=ProductFilterInput(), order_by=graphene.String())
+    all_orders = DjangoFilterConnectionField(OrderType, filterset_class=OrderFilter)
     
-    def resolve_all_products(self, info):
-        return Product.objects.all()
+    # Legacy simple list queries
+    customers = graphene.List(CustomerType, filter=CustomerFilterInput())
+    products = graphene.List(ProductType, filter=ProductFilterInput())
+    orders = graphene.List(OrderType, filter=OrderFilterInput())
+
+    def resolve_all_customers(self, info, filter=None, **kwargs):
+        queryset = Customer.objects.all()
+        if filter:
+            # Convert camelCase to snake_case for filter fields
+            converted_filter = {}
+            for key, value in filter.items():
+                if key == 'nameIcontains':
+                    converted_filter['name_icontains'] = value
+                elif key == 'emailIcontains':
+                    converted_filter['email_icontains'] = value
+                elif key == 'createdAtGte':
+                    converted_filter['created_at_gte'] = value
+                elif key == 'createdAtLte':
+                    converted_filter['created_at_lte'] = value
+                elif key == 'phonePattern':
+                    converted_filter['phone_pattern'] = value
+                else:
+                    converted_filter[key] = value
+            
+            customer_filter = CustomerFilter(converted_filter, queryset=queryset)
+            return customer_filter.qs
+        return queryset
+    
+    def resolve_all_products(self, info, filter=None, order_by=None, **kwargs):
+        queryset = Product.objects.all()
+        if filter:
+            # Convert camelCase to snake_case for filter fields
+            converted_filter = {}
+            for key, value in filter.items():
+                if key == 'priceGte':
+                    converted_filter['price_gte'] = value
+                elif key == 'priceLte':
+                    converted_filter['price_lte'] = value
+                elif key == 'stockGte':
+                    converted_filter['stock_gte'] = value
+                elif key == 'stockLte':
+                    converted_filter['stock_lte'] = value
+                elif key == 'nameIcontains':
+                    converted_filter['name_icontains'] = value
+                elif key == 'lowStock':
+                    converted_filter['low_stock'] = value
+                else:
+                    converted_filter[key] = value
+            
+            product_filter = ProductFilter(converted_filter, queryset=queryset)
+            queryset = product_filter.qs
+        if order_by:
+            queryset = queryset.order_by(order_by)
+        return queryset
+    
+    def resolve_customers(self, info, filter=None):
+        queryset = Customer.objects.all()
+        if filter:
+            customer_filter = CustomerFilter(filter, queryset=queryset)
+            return customer_filter.qs
+        return queryset
+    
+    def resolve_products(self, info, filter=None):
+        queryset = Product.objects.all()
+        if filter:
+            product_filter = ProductFilter(filter, queryset=queryset)
+            return product_filter.qs
+        return queryset
+    
+    def resolve_orders(self, info, filter=None):
+        queryset = Order.objects.all()
+        if filter:
+            order_filter = OrderFilter(filter, queryset=queryset)
+            return order_filter.qs
+        return queryset
 
 # -- Mutations --
 # Customer Mutation
